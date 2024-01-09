3c3ef6e36b533aa1103df57b888b10404e0f603d
johns j <johns643212@gmail.com>
2024-01-10 01:12:26 +0530

    Data validation updates

---
+++ script.js
@@ -0,0 +1,109 @@
+$(function(){
+  var $form=$("#register");
+  $form.validate({
+      rules:{
+          username:{
+              required:true,
+              lettersonly:true,
+              minlength:4
+          },
+          phone:{
+              required:true,
+              numbersonly:true,
+              minlength:10
+          },
+          email:{
+              required:true,
+             // email:true,
+              emailvalue:true
+              
+          },
+          password1:{
+              required:true,
+              all:true,
+              canumltrp:true
+          },
+          password2:{
+              required:true,
+              equalTo:"#password",
+              canumltrp:true
+          }
+      },
+      messages:{
+          username:{
+              required:'username requied',
+              lettersonly:'invalid name',
+              minlength:'Atleast 4 letters required' 
+          },
+          phone:{
+              required:'phone number required',
+              numbersonly:'invalid ',
+              minlength:'minimum 10 numbers required'
+          },
+          email:{
+              required:'Email required',
+              //email:'Email invalid',
+              emailvalue:'Email mismatch'
+          },
+          password1:{
+              required:'Password required',
+              all:'Space is not allowed',
+              canumltrp:'min 1 uppercase,number,lowercase required'
+
+          },
+          password2:{
+              required:'Conform password reqired',
+              equalTo:'Password mmismatch',
+              canumltrp:'min 1 uppercase,number,lowercase required'
+          }
+      }       
+  })
+  jQuery.validator.addMethod('lettersonly', function(value, element) {
+      return /^[^-\s][a-zA-Z_\s-]+$/.test(value);
+  });
+  jQuery.validator.addMethod('emailvalue', function(value, element) {
+      return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(value);
+  });
+  jQuery.validator.addMethod('numbersonly', function(value, element) {
+      return /^[0-9]+$/.test(value);
+  });
+
+  jQuery.validator.addMethod('all', function(value, element) {
+      return /^[^-\s][a-zA-Z0-9_\s-]+$/.test(value);
+  });
+
+  jQuery.validator.addMethod('canumltrp', function(value, element) {
+      return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(value);
+  });
+})
+$(function(){
+  var $form=$("#loginform");
+  $form.validate({
+      rules:{
+          username:{
+              required:true,
+              lettersonly:true
+          },
+          password1:{
+              required:true,
+              all:true
+          },
+      },
+      messages:{
+          username:{
+              required:'username requied',
+              lettersonly:'invalid name'  
+          },
+          password1:{
+              required:'Password required',
+              all:'Space is not allowed'
+          },
+      }       
+  })
+  jQuery.validator.addMethod('lettersonly', function(value, element) {
+      return /^[^-\s][a-zA-Z_\s-]+$/.test(value);
+  });
+  jQuery.validator.addMethod('all', function(value, element) {
+      return /^[^-\s][a-zA-Z0-9_\s-]+$/.test(value);
+  });
+})
